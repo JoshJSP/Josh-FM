e735efa7dcd1300a1d398eaa37ac7e87e3c73a8b
JoshJSP <76941723+JoshJSP@users.noreply.github.com>
2026-08-11 09:34:23 +0200

    Trigger deployment for corrected Dutch MMS tokenizer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='josh-fm-v3-neural-pages-4';
+const CACHE='josh-fm-v3-neural-pages-5';
 const CORE=['./','./index.html','./styles.css','./app.js','./bugfix-playback.js','./start-sequence.js','./dj-resume.js','./discovery.js','./debug-tts.js?v=mms-nl-pages-v3-20260811','./local-voice.js?v=mms-nl-pages-v3-20260811','./director.js','./smart-dj.js','./radio-suite.js','./radio-upgrades.js','./spotify-recovery.js','./dj-now-queue.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
