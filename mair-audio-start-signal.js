fe7ed5d33779be2de27f06eee2cb83afae9b0215
JoshJSP <76941723+JoshJSP@users.noreply.github.com>
2026-08-16 03:58:05 +0200

    Ignore silent WebAudio unlock buffers for DJ live state

--- mair-audio-start-signal.js
+++ mair-audio-start-signal.js
@@ -6,6 +6,7 @@ function emit(type,target,route,extra={}){try{window.dispatchEvent(new CustomEve
 function newToken(target){if(!target.__mairVoiceToken)target.__mairVoiceToken=++seq;return target.__mairVoiceToken}
 function clearToken(target){if(target)target.__mairVoiceToken=0}
 function isVoiceBlob(a){return /^blob:/i.test(String(a?.currentSrc||a?.src||''))}
+function isVoiceBuffer(source){const duration=Number(source?.buffer?.duration||0);return !!window.JFMDJAudio?.context&&source?.context===window.JFMDJAudio.context&&duration>=.05}
 const NativeAudio=window.Audio;
 if(typeof NativeAudio==='function'){
   function decorate(a){if(!a||a.__mairVoiceSignalBound)return a;a.__mairVoiceSignalBound=true;
@@ -22,12 +23,12 @@ if(sourceProto&&typeof sourceProto.start==='function'&&!sourceProto.__mairVoiceS
   const nativeStart=sourceProto.start;
   Object.defineProperty(sourceProto,'__mairVoiceStartWrapped',{value:true,configurable:true});
   sourceProto.start=function(...args){
-    const isVoiceContext=!!window.JFMDJAudio?.context&&this.context===window.JFMDJAudio.context;
-    if(isVoiceContext&&!this.__mairVoiceSignalBound){this.__mairVoiceSignalBound=true;this.addEventListener?.('ended',()=>{if(this.__mairVoiceToken)emit('mair:voice-playback-end',this,'web-audio');clearToken(this)},{once:true})}
+    const voice=isVoiceBuffer(this);
+    if(voice&&!this.__mairVoiceSignalBound){this.__mairVoiceSignalBound=true;this.addEventListener?.('ended',()=>{if(this.__mairVoiceToken)emit('mair:voice-playback-end',this,'web-audio');clearToken(this)},{once:true})}
     const result=nativeStart.apply(this,args);
-    if(isVoiceContext){newToken(this);emit('mair:voice-playback-start',this,'web-audio')}
+    if(voice){newToken(this);emit('mair:voice-playback-start',this,'web-audio')}
     return result
   }
 }
-window.MAIRAudioStartSignal={version:'mair-audio-start-signal-v2',routes:['html-audio-playing-event','web-audio-buffer-start']};
+window.MAIRAudioStartSignal={version:'mair-audio-start-signal-v2.1-audible-buffer-only',routes:['html-audio-playing-event','web-audio-buffer-start']};
 })();
